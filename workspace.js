ce0ef5aec0ff5a43b4b0b1da2483e661eb2bc21c
EC2 Default User <ec2-user@ip-172-31-21-247.us-east-2.compute.internal>
2018-04-15 15:05:38 +0000

    Made some changes to ChiliPeppr myWorkspace using Cloud9

--- workspace.js
+++ workspace.js
@@ -70,6 +70,7 @@ cpdefine("inline:com-chilipeppr-workspace-Ox-CNC-Grbl", ["chilipeppr_ready"], fu
             });
             
             this.loadTemplateWidget();
+            this.loadLuaEditorWidget();
             
             // Create our workspace upper right corner triangle menu
             this.loadWorkspaceMenu();
@@ -202,6 +203,28 @@ cpdefine("inline:com-chilipeppr-workspace-Ox-CNC-Grbl", ["chilipeppr_ready"], fu
                         }
                     );
                 }
+            );
+        },
+        /**
+         * Load the Lua Editor widget via chilipeppr.load()
+         */
+        loadLuaEditorWidget: function(callback) {
+            var that = this;
+            chilipeppr.load(
+              "#com-chilipeppr-widget-luaeditor-instance",
+              "http://raw.githubusercontent.com/chilipeppr/widget-luaeditor/master/auto-generated-widget.html",
+              function() {
+                // Callback after widget loaded into #myDivWidgetLuaeditor
+                // Now use require.js to get reference to instantiated widget
+                cprequire(
+                  ["inline:com-chilipeppr-widget-luaeditor"], // the id you gave your widget
+                  function(myObjWidgetLuaeditor) {
+                    // Callback that is passed reference to the newly loaded widget
+                    console.log("Widget / Lua Editor just got loaded.", myObjWidgetLuaeditor);
+                    myObjWidgetLuaeditor.init();
+                  }
+                );
+              }
             );
         },
         /**
